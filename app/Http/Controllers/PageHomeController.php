1dd510e87f03cb7b1d29178ae00592fb527081ba
phoelapyae <zinmyomaung17@gmail.com>
2024-05-05 01:40:13 +0700

    show course overview

---
+++ app/Http/Controllers/PageHomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Course;
+
+class PageHomeController extends Controller
+{
+    public function __invoke()
+    {
+        $courses = Course::query()
+        ->whereNotNull('released_at')
+        ->orderBy('released_at', 'desc')
+        ->get();
+
+        return view('home', [
+            'courses' => $courses
+        ]);
+    }
+}
--- tests/Feature/PagesResponseTest.php
+++ tests/Feature/PagesResponseTest.php
@@ -1,6 +1,10 @@
 <?php
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
 use function Pest\Laravel\get;
+
+uses(RefreshDatabase::class);
 
 test('give back successful response from home page', function () {
     get(route('home'))->assertOk();
